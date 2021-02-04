3548bc4aec8b26f669e3686da433ab787bea09bf
caleb-santos <caleb.santos.dev@gmail.com>
2021-02-04 20:33:44 -0300

    gitPlugin not ready

--- .zsh.after/gitPlugins.zsh
+++ .zsh.after/gitPlugins.zsh
@@ -1,4 +1,6 @@
-for zshPlugin in $HOME/.zshDotfiles/zshAfterGitPlugins/*/*.zsh; do
-  #echo $zshPlugin
-  source $zshPlugin
-done
+#if [ -f $HOME/.zshDotfiles/zshAfterGitPlugins/*/*.zsh ]; then
+#  for zshPlugin in $HOME/.zshDotfiles/zshAfterGitPlugins/*/*.zsh; do
+#    #echo $zshPlugin
+#    source $zshPlugin
+#  done
+#fi
